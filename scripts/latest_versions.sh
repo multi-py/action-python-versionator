75b93ebe2ee69a954b199647aea4314bd5f2e5d3
Robert Hafner <tedivm@tedivm.com>
2021-10-24 15:00:32 -0500

    Revert "output curl to see what's up"
    This reverts commit e1d1d89b35d0cab8a6e2f74b9f6bbb86594b64ed.

--- scripts/latest_versions.sh
+++ scripts/latest_versions.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-curl -sL https://pypi.org/simple/$1/
-
 VERSIONS=$(curl -sL https://pypi.org/simple/$1/ | grep -Eoi '>[a-zA-Z]+-.+?\.tar\.gz<' | grep -Eoi '\d+(\.\d+)+((a|b|rc)\d?)?' | tail -${2:-10})
 echo $VERSIONS
