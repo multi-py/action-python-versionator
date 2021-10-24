1c319a8a49f9a686fb04041307e90fc2b9dcc981
Robert Hafner <tedivm@tedivm.com>
2021-10-24 15:14:59 -0500

    fix latest_versions

--- scripts/latest_versions.sh
+++ scripts/latest_versions.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-VERSIONS=$(curl -sL https://pypi.org/simple/$1/ | grep -Eoi '>[a-zA-Z]+-.+?\.tar\.gz<' | grep -Eoi '\d+(\.\d+)+((a|b|rc)\d?)?' | tail -${2:-10})
+# Silly OSX
+if which ggrep > /dev/null; then
+  GREP=ggrep
+else
+  GREP=grep
+fi
+
+VERSIONS=$(curl -sL https://pypi.org/simple/uvicorn/ | $GREP -oh '[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\.tar\.gz<' | cut -d"." -f 1,2,3 | tail -${2:-10})
 echo $VERSIONS
