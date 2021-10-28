3f2eb6307e3fd95274028859dacdbdbd6e7fea9c
Robert Hafner <tedivm@tedivm.com>
2021-10-27 19:58:12 -0500

    Add ability to exclude versions from build

--- scripts/latest_versions.py
+++ scripts/latest_versions.py
@@ -6,6 +6,7 @@ import urllib.request
 
 PACKAGE = sys.argv[1]
 MAX_VERSIONS = sys.argv[2] if len(sys.argv) > 2 else 10
+EXCLUDE_VERSIONS=os.environ.get("EXCLUDE_VERSIONS", "").split(",")
 
 # Load PyPI website for the package
 fp = urllib.request.urlopen(f"https://pypi.org/simple/{PACKAGE}/")
@@ -15,10 +16,10 @@ fp.close()
 # Pick a regex- either include prerelease versions or not
 if os.environ.get("INCLUDE_PRERELEASE", False) == 'true':
   version_regex = re.compile(r'-(\d+\.\d+\.\d+(a|b|rc)?\d?).+[\.whl|\.tar\.gz]')
-  versions = [x[0] for x in version_regex.findall(html_string)]
+  versions = [x[0] for x in version_regex.findall(html_string) if x[0] not in EXCLUDE_VERSIONS]
 else:
   version_regex = re.compile(r'-(\d+\.\d+\.\d+).+[\.whl|\.tar\.gz]')
-  versions = version_regex.findall(html_string)
+  versions = [x for x in version_regex.findall(html_string) if x not in EXCLUDE_VERSIONS]
 
 unique_versions = list(dict.fromkeys(versions))
 
